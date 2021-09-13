60a64196ede1d4db3bd58a41af418816cfee6089
RobzLegz <robzlegz@gmail.com>
2021-09-13 17:29:59 +0300

    whitelists

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const io = require("socket.io")(8900, {
     cors: {
-        origin: "http://localhost:3000/"
+        origin: "http://localhost:3000",
+        origin: "https://youthdeal-frontend.vercel.app"
     }
 });
 
