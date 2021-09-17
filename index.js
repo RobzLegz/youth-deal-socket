b70509da37d2190c9198b8df716b1b53562c97b7
Rob <robzlegz@gmail.com>
2021-09-17 23:53:27 +0300

    dotenv

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-  
-const io = require("socket.io")(8900, {
-    cors: {
-      origin: "http://localhost:3000",
-    },
-  });
+require("dotenv").config();
+const io = require("socket.io")(process.env.PORT, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
   
   let users = [];
   
